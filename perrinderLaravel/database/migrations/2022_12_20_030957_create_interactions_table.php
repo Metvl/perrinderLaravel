bbd478ad490430477c42ee23f9abc5dd3737584e
Metvl <ojedacasanueva@gmail.com>
2022-12-20 00:45:51 -0300

    First commit, create migrations, seeders, factory

---
+++ perrinderLaravel/database/migrations/2022_12_20_030957_create_interactions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('interactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_interested_dog');
+            $table->unsignedBigInteger('id_candidate_dog');
+            $table->string('preference', 100)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('interactions');
+    }
+};
